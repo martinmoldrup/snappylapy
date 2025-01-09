9a481ef5eb7d8761cd06198e2b5c66c5f65cb96e
Martin Møldrup <14809761+martinmoldrup@users.noreply.github.com>
2025-01-09 10:55:52 +0100

    Enable stricter ruff linting checks

--- snappylapy/plugin.py
+++ snappylapy/plugin.py
@@ -1,63 +1,65 @@
-"""
-Pytest plugin for snapshot testing.
-"""
-from typing import List
+"""Pytest plugin for snapshot testing."""
 import pytest
 from snappylapy import Expect, LoadSnapshot
 from snappylapy.snappylapy import Settings
 
 
-
-@pytest.fixture
-def expect(request) -> Expect:
+@pytest.fixture()
+def expect(request: pytest.FixtureRequest) -> Expect:
     """Initialize the snapshot object with update_snapshots flag from pytest option."""
     update_snapshots = request.config.getoption("--snapshot-update")
     return Expect(update_snapshots=update_snapshots)
 
-@pytest.fixture
+
+@pytest.fixture()
 def load_snapshot(request: pytest.FixtureRequest) -> LoadSnapshot:
     """Initialize the LoadSnapshot object."""
     marker = request.node.get_closest_marker("snappylapy")
     depends = marker.kwargs.get("depends", []) if marker else []
     if not depends:
         return LoadSnapshot(Settings())
-    
+
     filename = depends[0].__name__
-    settings = Settings(
-        filename_base=filename,
-    )
+    settings = Settings(filename_base=filename)
     return LoadSnapshot(settings)
 
-class SnappylapyPlugin:
-    """Plugin for sorting the tests based on the dependencies."""
-    @pytest.hookimpl(tryfirst=True)
-    def pytest_collection_modifyitems(self, config: pytest.Config, items: List[pytest.Function]):
-        for item in items:
-            marker = item.get_closest_marker("snappylapy")
-            if not marker:
-                continue
-            depends = marker.kwargs.get("depends", [])
-            for depend in depends:
-                for i, test in enumerate(items):
-                    if test.function != depend:
-                        continue
-                    # Check if it is already earlier in the list than the dependency
-                    if i < items.index(item):
-                        # Preserve the original order
-                        break
-                    # Move the test to the position after the dependency
-                    items.insert(i + 1, items.pop(items.index(item)))
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_collection_modifyitems(
+    session: pytest.Session,
+    config: pytest.Config,
+    items: list[pytest.Function],
+) -> None:
+    """Sort the tests based on the dependencies."""
+    del config, session  # Unused
+    for item in items:
+        marker = item.get_closest_marker("snappylapy")
+        if not marker:
+            continue
+        depends = marker.kwargs.get("depends", [])
+        for depend in depends:
+            for i, test in enumerate(items):
+                if test.function != depend:
+                    continue
+                # Check if it is already earlier in the list than the dependency
+                if i < items.index(item):
+                    # Preserve the original order
                     break
+                # Move the test to the position after the dependency
+                items.insert(i + 1, items.pop(items.index(item)))
+                break
 
-def pytest_configure(config: pytest.Config):
-    """
-    Register the "snappylapy" marker and configure the plugin,
-    depending on the CLI options.
-    """
-    config.addinivalue_line("markers", "snappylapy: mark test to load snapshot data from a file.")
-    config.pluginmanager.register(SnappylapyPlugin())
 
-def pytest_addoption(parser):
+def pytest_configure(config: pytest.Config) -> None:
+    """Register the markers used."""
+    config.addinivalue_line(
+        "markers",
+        "snappylapy: mark test to load snapshot data from a file.",
+    )
+
+
+def pytest_addoption(parser: pytest.Parser) -> None:
+    """Add the CLI option for updating snapshots."""
     group = parser.getgroup("terminal reporting")
     group.addoption(
         "--snapshot-update",
@@ -66,7 +68,3 @@ def pytest_addoption(parser):
         default=False,
         help="update snapshots.",
     )
-
-
-
-
--- snappylapy/snappylapy.py
+++ snappylapy/snappylapy.py
@@ -1,14 +1,18 @@
-"""Jest style testing"""
-from typing import Any, TypeVar, Generic, List, Optional, Union
-from abc import ABC, abstractmethod
-import pathlib
+"""Snapshot testing for Python."""
+from __future__ import annotations
+
 import inspect
+import pathlib
+from .serialization import BytesSerializer, JsonPickleSerializer, StringSerializer
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from .serialization import JsonPickleSerializer, StringSerializer, BytesSerializer
+from typing import Any, Generic, TypeVar
+
 
 @dataclass
 class Settings:
     """Shared setting for all the strategies for doing snapshot testing."""
+
     snapshot_dir: pathlib.Path = pathlib.Path("__snapshots__")
     test_results_dir: pathlib.Path = pathlib.Path("__test_results__")
     snapshot_update: bool = False
@@ -21,20 +25,27 @@ class Settings:
         return f"{self.filename_base}.{self.filename_extension}"
 
 
-T = TypeVar('T')
+T = TypeVar("T")
+
+
 class BaseSnapshot(ABC, Generic[T]):
     """Base class for snapshot testing."""
 
-    def __init__(self, update_snapshots: bool, settings: Settings) -> None:
+    def __init__(
+        self,
+        update_snapshots: bool,  # noqa: FBT001
+        settings: Settings,
+    ) -> None:
         """Initialize the base snapshot."""
         self.settings = settings
         self.snapshot_update: bool = update_snapshots
-        self._data: Optional[T] = None
+        self._data: T | None = None
 
     def to_match_snapshot(self) -> None:
         """Assert test results match the snapshot."""
         if not self._compare_snapshot():
-            raise AssertionError("Test results do not match the snapshot.")
+            error_msg = "Test results do not match the snapshot."
+            raise AssertionError(error_msg)
 
     def _prepare_test(self, data: T, name: str, extension: str) -> None:
         """Prepare and save test results."""
@@ -49,12 +60,16 @@ class BaseSnapshot(ABC, Generic[T]):
 
     def _compare_snapshot(self) -> bool:
         """Compare the snapshot with test results, updating if needed."""
-        if not self.snapshot_update and not (self.settings.snapshot_dir / self.settings.filename).exists():
-            raise FileNotFoundError(f"Snapshot file not found: {self.settings.filename}, run pytest with the --snapshot-update flag to create it.")
+        if not self.snapshot_update and not (self.settings.snapshot_dir /
+                                             self.settings.filename).exists():
+            error_msg = f"Snapshot file not found: {self.settings.filename}, run pytest with the --snapshot-update flag to create it."  # noqa: E501
+            raise FileNotFoundError(error_msg)
         if self.snapshot_update:
             self._update_snapshot()
-        snapshot_data = self._read_file(self.settings.snapshot_dir / self.settings.filename)
-        test_data = self._read_file(self.settings.test_results_dir / self.settings.filename)
+        snapshot_data = self._read_file(self.settings.snapshot_dir /
+                                        self.settings.filename)
+        test_data = self._read_file(self.settings.test_results_dir /
+                                    self.settings.filename)
         return snapshot_data == test_data
 
     def _update_snapshot(self) -> None:
@@ -75,7 +90,8 @@ class BaseSnapshot(ABC, Generic[T]):
             if frame.f_code.co_filename != __file__:
                 return frame.f_code.co_name
             frame = frame.f_back
-        raise ValueError("Could not derive filename from stack.")
+        error_msg = "Could not derive filename from stack."
+        raise ValueError(error_msg)
 
     @abstractmethod
     def _save_test_results(self, path: pathlib.Path, data: T) -> None:
@@ -85,9 +101,11 @@ class BaseSnapshot(ABC, Generic[T]):
 
 class DictExpect(BaseSnapshot[dict]):
     """Snapshot testing for dictionaries."""
-    def __call__(self, data_to_snapshot: dict,
+
+    def __call__(self,
+                 data_to_snapshot: dict,
                  name: str = "",
-                 filetype="dict.json") -> "DictExpect":
+                 filetype: str = "dict.json") -> DictExpect:
         """Prepare a dictionary for snapshot testing."""
         self._prepare_test(data_to_snapshot, name, filetype)
         return self
@@ -97,40 +115,54 @@ class DictExpect(BaseSnapshot[dict]):
         data_bin = JsonPickleSerializer[dict]().serialize(data)
         path.write_bytes(data_bin)
 
-class ListExpect(BaseSnapshot[List[Any]]):
+
+class ListExpect(BaseSnapshot[list[Any]]):
     """Snapshot testing for lists."""
-    def __call__(self, data_to_snapshot: List[Any],
-                 name: str = "",
-                 filetype="list.json") -> "ListExpect":
+
+    def __call__(
+        self,
+        data_to_snapshot: list[Any],
+        name: str = "",
+        filetype: str = "list.json",
+    ) -> ListExpect:
         """Prepare a list for snapshot testing."""
         self._prepare_test(data_to_snapshot, name, filetype)
         return self
 
-    def _save_test_results(self, path: pathlib.Path, data: List[Any]) -> None:
+    def _save_test_results(
+        self,
+        path: pathlib.Path,
+        data: list[Any],
+    ) -> None:
         """Save list data to a file."""
-        data_bin = JsonPickleSerializer[List[Any]]().serialize(data)
+        data_bin = JsonPickleSerializer[list[Any]]().serialize(data)
         path.write_bytes(data_bin)
+
 
 class StringExpect(BaseSnapshot[str]):
     """Snapshot testing for strings."""
 
-    def __call__(self, data_to_snapshot: str,
-                    name: str = "",
-                    filetype="string.txt") -> "StringExpect":
-            """Prepare a string for snapshot testing."""
-            self._prepare_test(data_to_snapshot, name, filetype)
-            return self
+    def __call__(self,
+                 data_to_snapshot: str,
+                 name: str = "",
+                 filetype: str = "string.txt") -> StringExpect:
+        """Prepare a string for snapshot testing."""
+        self._prepare_test(data_to_snapshot, name, filetype)
+        return self
 
     def _save_test_results(self, path: pathlib.Path, data: str) -> None:
         """Save string data to a file."""
         data_bin = StringSerializer().serialize(data)
         path.write_bytes(data_bin)
 
+
 class BytesExpect(BaseSnapshot[bytes]):
     """Snapshot testing for bytes."""
-    def __call__(self, data_to_snapshot: bytes,
+
+    def __call__(self,
+                 data_to_snapshot: bytes,
                  name: str = "",
-                 filetype="bytes.txt") -> "BytesExpect":
+                 filetype: str = "bytes.txt") -> BytesExpect:
         """Prepare bytes for snapshot testing."""
         self._prepare_test(data_to_snapshot, name, filetype)
         return self
@@ -143,7 +175,12 @@ class BytesExpect(BaseSnapshot[bytes]):
 
 class Expect:
     """Snapshot testing class."""
-    def __init__(self, update_snapshots: bool = False) -> None:
+
+    def __init__(
+            self,
+            update_snapshots: bool,  # noqa: FBT001
+    ) -> None:
+        """Initialize the snapshot testing."""
         self.settings = Settings()
         self.dict = DictExpect(update_snapshots, self.settings)
         self.list = ListExpect(update_snapshots, self.settings)
@@ -152,47 +189,59 @@ class Expect:
 
     def read_snapshot(self) -> bytes:
         """Read the snapshot file."""
-        return (self.settings.snapshot_dir / self.settings.filename).read_bytes()
-    
+        return (self.settings.snapshot_dir /
+                self.settings.filename).read_bytes()
+
     def read_test_results(self) -> bytes:
         """Read the test results file."""
-        return (self.settings.test_results_dir / self.settings.filename).read_bytes()
+        return (self.settings.test_results_dir /
+                self.settings.filename).read_bytes()
 
-    
     @property
     def snapshot_dir(self) -> pathlib.Path:
+        """Get the snapshot directory."""
         return self.settings.snapshot_dir
 
     @snapshot_dir.setter
-    def snapshot_dir(self, value: Union[str, pathlib.Path]) -> None:
-        self.settings.snapshot_dir = pathlib.Path(value) if isinstance(value, str) else value
+    def snapshot_dir(self, value: str | pathlib.Path) -> None:
+        """Set the snapshot directory."""
+        self.settings.snapshot_dir = pathlib.Path(value) if isinstance(
+            value, str) else value
 
     @property
     def test_results_dir(self) -> pathlib.Path:
+        """Get the test results directory."""
         return self.settings.test_results_dir
 
     @test_results_dir.setter
-    def test_results_dir(self, value: Union[str, pathlib.Path]) -> None:
-        self.settings.test_results_dir = pathlib.Path(value) if isinstance(value, str) else value
+    def test_results_dir(self, value: str | pathlib.Path) -> None:
+        """Set the test results directory."""
+        self.settings.test_results_dir = pathlib.Path(value) if isinstance(
+            value, str) else value
+
 
 class LoadSnapshot:
     """Snapshot loading class."""
+
     def __init__(self, settings: Settings) -> None:
+        """Initialize the snapshot loading."""
         self.settings = settings
 
     def _read_snapshot(self) -> bytes:
         """Read the snapshot file."""
-        return (self.settings.snapshot_dir / self.settings.filename).read_bytes()
+        return (self.settings.snapshot_dir /
+                self.settings.filename).read_bytes()
 
     def dict(self) -> dict:
         """Load dictionary snapshot."""
         self.settings.filename_extension = "dict.json"
         return JsonPickleSerializer[dict]().deserialize(self._read_snapshot())
 
-    def list(self) -> List[Any]:
+    def list(self) -> list[Any]:
         """Load list snapshot."""
         self.settings.filename_extension = "list.json"
-        return JsonPickleSerializer[List[Any]]().deserialize(self._read_snapshot())
+        return JsonPickleSerializer[list[Any]]().deserialize(
+            self._read_snapshot())
 
     def string(self) -> str:
         """Load string snapshot."""
