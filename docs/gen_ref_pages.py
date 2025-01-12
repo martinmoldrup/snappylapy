7150fdc32967af22162cab95333e09e9f7c8a00d
Martin Møldrup <14809761+martinmoldrup@users.noreply.github.com>
2025-01-12 15:05:08 +0100

    Add makedocs automatically generating the references

---
+++ docs/gen_ref_pages.py
@@ -0,0 +1,30 @@
+"""Generate the code reference pages and navigation for documentation with MKDocs."""
+
+import mkdocs_gen_files
+from pathlib import Path
+
+nav = mkdocs_gen_files.Nav()
+
+root = Path(__file__).parent.parent
+src = root / "snappylapy"
+
+for path in sorted(src.rglob("*.py")):
+    doc_path = path.relative_to(src.parent).with_suffix(".md")
+    full_doc_path = Path("reference", doc_path)
+    parts = list(path.relative_to(src.parent).with_suffix("").parts)
+    if path.stem == "__init__":
+        doc_path = doc_path.with_name("index.md")
+        full_doc_path = full_doc_path.with_name("index.md")
+        # delete the "__init__" element
+        parts.remove("__init__")
+
+    nav[parts] = doc_path.as_posix()
+
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        ident = ".".join(parts)
+        fd.write(f"::: {ident}")
+
+    mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
+
+with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+    nav_file.writelines(nav.build_literate_nav())
